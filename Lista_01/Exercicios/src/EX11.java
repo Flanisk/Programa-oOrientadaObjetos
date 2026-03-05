d413b51306fbab19c30d48bf3c4d6d5660a08827
Flávio Rocha Lamberti <156143187+Flanisk@users.noreply.github.com>
2026-03-05 19:59:48 -0300

    Add files via upload

---
+++ Lista_01/Exercicios/src/EX11.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class EX11 {
+    public static void main (String [] args){
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Me fale o tamanho da raiz quadrada: ");
+        int n = leia.nextInt();
+
+        int matriz[][] = new int[n][n];
+
+        for (int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                matriz[i][j] = (int)(Math.random() * 100);
+
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                System.out.print(matriz[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Diagonal principal: ");
+        for(int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < matriz[i].length; j++){
+                if(i >= j){
+                    System.out.print(matriz[i][j] + " ");
+
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+}
